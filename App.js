312dd843b55c21f1bd6f27101a95ba27d241d518
raphaelrocha <hifa73191105>
2019-10-20 18:27:06 -0400

    back ground no nivel app

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View, StatusBar} from 'react-native';
+import {View, StatusBar, Image, StyleSheet} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Profile from "./src/modules/profile/Profile";
 import SendMoney from "./src/modules/sendMoney/SendMoney";
 import colors from "./src/constants/colors";
 import TransferHistory from "./src/modules/transferHistory/TransferHistory";
+import images from "./src/assets/images";
 
 console.disableYellowBox = true;
 
@@ -22,8 +23,27 @@ const MainNavigator = createStackNavigator({
 
 const AppContent = createAppContainer(MainNavigator);
 
+const styles = StyleSheet.create({
+    rootView:{
+        flex:1,
+    },
+    background:{
+        tintColor: colors.BLUE_GREY_900,
+        height: '100%',
+        width: '100%',
+        position: 'absolute',
+        top:0,
+        left:0,
+        zIndex:0,
+    },
+});
+
 export default App = () =>
-    <View style={{flex: 1}}>
+    <View style={styles.rootView}>
+        <Image
+            style={styles.background}
+            source={images.bgGrad}
+        />
       <StatusBar barStyle="light-content" backgroundColor={colors.BLUE_GREY_900}/>
       <AppContent />
     </View>;
